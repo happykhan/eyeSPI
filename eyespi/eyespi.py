387f3dcd5a3c5b7353208f622bdc8b77896ad16f
Nabil-Fareed Alikhan <nabil@happykhan.com>
2020-03-06 11:38:12 +0000

    basic CLI

--- eyespi/eyespi.py
+++ eyespi/eyespi.py
@@ -4,60 +4,61 @@ import sys
 import getopt
 import subprocess
 from Bio import SeqIO
+import argparse
+import logging
 
+log = logging.getLogger()
 
-def main(argv):
-    """"This script removes a target sequence from an input fasta based on a blastn search"""
-    inputfile = ''  # type: str
-    matchfile = ''  # type: str
-    identity = ''   # type: int
-    match_length = ''  # type: int
-    outputfile = ''
-    try:
-        opts, args = getopt.getopt(argv, "hi:c:x:m:o:", ["ifile=", "cfile=", "ival1=", "ival2=", "ofile="])
-    except getopt.GetoptError:
-        print ('presence-cut.py -i <inputfile> -c <matchfile> -x <identity> -m <match_length> -o <outputfile>')
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            print ('presence.py -i <inputfile> -c <matchfile> -x <identity> -m <match_length>')
-            sys.exit()
-        elif opt in ("-i", "--ifile"):
-            inputfile = arg
-        elif opt in ("-c", "--cfile"):
-            matchfile = arg
-        elif opt in ("-x", "--ival1"):
-            identity = arg
-        elif opt in ("-m", "--ival2"):
-            match_length = arg
-        elif opt in ("-o", "--ofile"):
-            outputfile = arg
-    print ('Input file is', inputfile)
-    print ('Sequence to search for is', matchfile)
-    print ('blast identity is', identity, '%')
-
+def run_blast(matchfile, inputfile, identity='90' ):
     command = ['blastn', '-query', matchfile,'-subject', inputfile,'-perc_identity', identity,
                '-outfmt', '6', '-max_target_seqs', '1']
-
+    log.debug(f"Running {' '.join(command)}")
     output = subprocess.Popen(command, stdout=subprocess.PIPE)
     output = output.communicate()[0]
     output = output.decode()
+    return output
+
+def check_startup():
+    return True
+
+def main(args):
+    """"This script removes a target sequence from an input fasta based on a blastn search"""
+    inputfile = args.ifile 
+    matchfile = args.cfile
+    identity = args.ival1
+    match_length = args.ival2
+    outputfile = args.output
+
+    # Testing 
+    inputfile = 'spi_seq/SPI-1.gbk'
+    matchfile = 'test-data/A130.fasta'
+    identity = '90'
+    # This should be a proportion of the ref sequence 
+    match_length = 90
+    outputfile = 'test.fna'
+    log.setLevel(logging.DEBUG)
+    ## 
+    if inputfile.endswith('.gbk'):
+        clean_input = inputfile + '.fna'
+        SeqIO.convert(inputfile, 'genbank', inputfile + '.fna', 'fasta')
+    else:
+        clean_input = inputfile
+    output = run_blast(matchfile, clean_input, identity)
     firstline = output.split("\n")[0]
     length_match = firstline.split("\t")[3]
     start_match = firstline.split("\t")[8]
     end_match = firstline.split("\t")[9]
 
-    if length_match > match_length:
-        print('SGI4 Present')
+    if int(length_match) > int(match_length):
         print('start =', start_match)
         print('end =', end_match)
         print('length =', length_match)
         if end_match > start_match:
-            record = SeqIO.read(inputfile, "fasta")
+            record = SeqIO.read(clean_input, "fasta")
             with open(outputfile, "w") as out:
                 SeqIO.write(record[:int(start_match) + 1] + record[int(end_match) + 1:], out, "fasta")
         elif start_match > end_match:
-            record = SeqIO.read(inputfile, "fasta")
+            record = SeqIO.read(clean_input, "fasta")
             with open(outputfile, "w") as out:
                SeqIO.write(record[:int(end_match) + 1] + record[int(start_match) + 1:], out, "fasta")
     elif length_match < match_length:
@@ -70,4 +71,13 @@ def main(argv):
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    if check_startup():
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-i', '--ifile', help='input file')
+        parser.add_argument('-c', '--cfile', help='match file')
+        parser.add_argument('-x', '--ival1', help='identity')        
+        parser.add_argument('-m', '--ival2', help='match length')  
+        parser.add_argument('-o', '--output', help='output')  
+        parser.add_argument('-v', '--verbose', help='verbose') 
+        args = parser.parse_args()
+        main(args)
